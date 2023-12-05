9deb0c2f7f14b35c8bbc0f4a0b3a80db7299e9ef
Barin Britva <barinbritva@outlook.com>
2023-12-05 17:17:18 +0300

    Fix assets ignore.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/scripts'),
   },
   devServer: {
     static: path.join(__dirname, 'dist'),
